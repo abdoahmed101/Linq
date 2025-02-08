4d30d720e9557e737128ad2a30e587ce8e9b33b7
Abdo Ahmed <Abdo Ahmed@DESKTOP-RE0MK7H>
2025-02-08 19:02:59 +0200

    Add project files.

---
+++ Assignment1/Digits.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal static class Integers
+    {
+        public static string [] DigitsOfArray(this string [] Arr)
+        {
+            List <string> results = new List <string> ();
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if(Arr[i].Length < i )
+                {
+                    results.Add($"{i}: {Arr[i]}");
+                }
+            }
+            return results.ToArray();
+        }
+
+        public static List<bool> InPlace(this int[] Arr)
+        {
+            List <bool> InPlace = new List<bool> ();
+            for (int i = 0; i < Arr.Length; i++)
+            { 
+                    InPlace.Add(Arr[i] == i);  
+            }
+            return InPlace;
+        }
+
+        public static List<(int, int)> Pairs(this int[] Arr , int[]Arr2)
+        {
+            List<(int, int)> Result = new List<(int,int)>();
+            for (int i = 0; i<Arr.Length; i++)
+            {
+                for(int j = 0; j<Arr2.Length; j++)
+                {
+                    if(Arr[i] < Arr2[j])
+                        Result.Add((Arr[i], Arr2[j]));
+                }
+            }
+            return Result;
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Products = ListGenerator.ProductList;
+            var Customers = ListGenerator.CustomerList;
+            // Fluent Syntax
+            // 1
+            //var ProductsOutOfStock = Products.Where(Product => Product.UnitsInStock == 0);
+            //foreach (var Product in ProductsOutOfStock)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+            //---------------
+            // 2
+            //var ProductsInStoreHigherThan3 = Products.Where(Product => Product.UnitsInStock != 0 && Product.UnitPrice > 3);
+            //foreach (var Product in ProductsInStoreHigherThan3)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+
+            // Query Syntax
+            // 1
+            //var ProductsOutOfStock = from P in Products
+            //                         where P.UnitsInStock == 0
+            //                         select P;
+
+            //foreach (var Product in ProductsOutOfStock)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+            // ------------
+            // 2 
+            //var ProductsInStoreHigherThan3 = from Product in Products
+            //                         where Product.UnitsInStock != 0 && Product.UnitPrice > 3
+            //                         select Product;
+
+            //foreach (var Product in ProductsInStoreHigherThan3)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //string[] Result = Arr.DigitsOfArray();
+            //for (int i = 0; i < Result.Length; i++)
+            //{
+            //    Console.WriteLine(Result[i]);
+            //}
+
+            // Return a sequence of just the names of a list of products.
+            //var ListOfProductsNames = Products.Select(Product => Product.ProductName);
+            //foreach (var Product in ListOfProductsNames)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+            //var ListOfProductsNames = from Product in Products
+            //                          select Product.ProductName;
+
+            //foreach (var Product in ListOfProductsNames)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+            //string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+
+            //var Result = words.Select(word => new
+            //{
+            //    UpperCase = word.ToUpper(),
+            //    LowerCase = word.ToLower(),
+            //});
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //var ProductsProperties = Products.Select(Product =>
+            //new
+            //{Product.ProductID ,
+            //Product.ProductName ,
+            //Price = Product.UnitPrice
+            //});
+
+            //foreach (var Product in ProductsProperties)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+
+            //var ProductsProperties = from Product in Products
+            //                         select new
+            //                         {
+            //                             Product.ProductID,
+            //                             Product.ProductName,
+            //                             Price = Product.UnitPrice
+            //                         };
+
+
+            //foreach (var Product in ProductsProperties)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //List<bool> R = Arr.InPlace();
+
+            //for (int i = 0; i < Arr.Length; i++)
+            //{
+            //    Console.WriteLine($"{Arr[i]}: {R[i]}");
+            //}
+
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+            //List <(int,int)> Result = new List <(int,int)> ();
+            //Result = numbersA.Pairs(numbersB);
+            //foreach (var pair in Result)
+            //{
+            //    Console.WriteLine($"{pair.Item1} is less than {pair.Item2}");
+            //}
+
+            //var ordersBelow500 = Customers.SelectMany(customer => customer.Orders)
+            //                               .Where(order => order.Total < 500);
+
+            //foreach (var Order in ordersBelow500)
+            //{
+            //    Console.WriteLine(Order);
+            //}
+
+            //var ordersBelow500 = from C in Customers
+            //                     from Order in C.Orders
+            //                     where Order.Total < 500
+            //                     select Order;
+
+            //foreach (var Order in ordersBelow500)
+            //{
+            //    Console.WriteLine(Order);
+            //}
+
+            //var OrderBy1998OrLater = Customers.SelectMany(Customer => Customer.Orders).Where(Order => Order.OrderDate >= new DateTime(1998 , 1 , 1));
+            //foreach (var Order in OrderBy1998OrLater)
+            //{
+            //    Console.WriteLine(Order);
+            //}
+
+           // var OrderBy1998OrLater = from C in Customers
+           //                          from Order in C.Orders
+           //                          where Order.OrderDate >= new DateTime(1998, 1, 1)
+           //                          select Order;
+
+           //foreach (var Order in OrderBy1998OrLater)
+           // {
+           //     Console.WriteLine(Order);
+           // }
+
+        }
+    }
+}
